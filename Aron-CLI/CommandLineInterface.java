d136db29d99a81b7ef973e84aa791189f5f68c10
adamfogarty <150444648+adamfogarty@users.noreply.github.com>
2023-12-03 21:27:37 +0000

    Added QCA calculation to transcipt

--- Aron-CLI/CommandLineInterface.java
+++ Aron-CLI/CommandLineInterface.java
@@ -739,6 +739,9 @@ public class CommandLineInterface {
                         transcript += moduleDetails + "\n";
                         moduleDetails = "";
                     }
+                    double qca = QCAcalculation(1, year.sem1);
+                    transcript += "QCA For Semester: " + String.format("%.2f", qca) + "\n";
+                    transcript += new String(new char[80]).replace("\0", "-") + "\n";
                 
                 moduleDetails += "Semester 2" + "\n";
                     for(int j = 0; j < year.sem2.size(); j++)
@@ -748,9 +751,84 @@ public class CommandLineInterface {
                         transcript += moduleDetails + "\n";
                         moduleDetails = "";
                     }
+                    qca = QCAcalculation(1, year.sem2);
+                    transcript += "QCA For Semester: " + String.format("%.2f", qca) + "\n";
+                    transcript += new String(new char[80]).replace("\0", "-") + "\n";
                 
-            }             
+            }     
         transcript += new String(new char[80]).replace("\0", "-");
         return transcript;
+    }
+    
+    public static double QCAcalculation(int factor, ArrayList<gradedCourseModule> set) {
+        double moduleQpv = 0;
+        double qcs = 0;
+        int nonQualityHours = 0;
+        int attemptedHours = 0;
+        int qpv = 0;
+        String grade = null;
+        int credits = 0;
+  
+        for (int i = 0; i< set.size(); i++) 
+        {
+            gradedCourseModule module = set.get(i);
+            grade = module.getGrade();
+            attemptedHours += (module.getModuleCredits() * factor);
+            switch (grade) {
+                case "A1":
+                    moduleQpv = 4.00;
+                    break;
+                case "A2":
+                    moduleQpv = 3.60;
+                    break;
+                case "B1":
+                    moduleQpv = 3.20;
+                    break;
+                case "B2":
+                    moduleQpv = 3.00;
+                    break;
+                case "B3":
+                    moduleQpv = 2.80;
+                    break;
+                case "C1":
+                    moduleQpv = 2.60;
+                    break;
+                case "C2":
+                    moduleQpv = 2.40;
+                    break;
+                case "C3":
+                    moduleQpv = 2.00;
+                    break;
+                case "D1":
+                    moduleQpv = 1.60;
+                    break;
+                case "D2":
+                    moduleQpv = 1.20;
+                    break;
+                case "F":
+                case "NG":
+                    moduleQpv = 0.00;
+                    break;
+                case "P":
+                case "I":
+                case "N":
+                case "H":
+                case "EX":
+                    nonQualityHours += (module.getModuleCredits() * factor);
+                    break;
+                case "M":
+                case "G":
+                case "R":
+                case "W":
+
+                    break;
+                default:
+                    System.out.println("Invalid grade"); 
+                    break;
+            }
+            credits= module.getModuleCredits();
+            qcs += (moduleQpv * credits * factor);
+        }
+        return (qcs / (attemptedHours - nonQualityHours));
     }
 }
